f7b5ea59f29e67dd522bda8ff7bef5d2d3d3a225
Priyansh <priyankakadlag>
2021-09-28 14:07:49 +0530

    finall demo

--- src/test/java/com/testingshastra/Keyword.java
+++ src/test/java/com/testingshastra/Keyword.java
@@ -1,5 +1,12 @@
 package com.testingshastra;
 
+import org.openqa.selenium.WebElement;
+
 public class Keyword {
+	public void clickOnElement(WebElement element) {
+		element.click();
+System.out.println("hi");
+
+	}
 
 }
